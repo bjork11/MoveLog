be78c756502bbdde79a9dac1cf013b1e79b4b0f2
AdamBjork <adamhbjork@gmail.com>
2019-11-20 13:10:11 +0100

    version 1

---
+++ ClassLibrary/Sport.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    abstract class Sport
+    {
+        protected string type;
+        public virtual double PacePerDistance(double distanceInMeters, double timeInSeconds)
+        {
+            double distanceInKm = (distanceInMeters / 1000);
+            double timeInMinutes = (timeInSeconds / 60);
+            double minutesPerKm = (timeInMinutes / distanceInKm);
+
+            return minutesPerKm;
+        }
+    }
+
+    class Walking : Sport
+    {
+        public Walking()
+        {
+            this.type = "Walking";
+        }
+    }
+
+    class Running : Sport
+    {
+        public Running()
+        {
+            this.type = "Running";
+        }
+    }
+
+    class Biking : Sport
+    {
+        public Biking()
+        {
+            this.type = "Biking";
+        }
+    }
+
+}
---
+++ ClassLibrary/User.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ClassLibrary
+{
+    class User
+    {
+        string firstName;
+        string lastName;
+        DateTime dateOfBirth;
+        string userName;
+        string password;
+
+    }
+}

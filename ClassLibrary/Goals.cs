ed2806d48e8555368ae7d9e86a2a1fef74314b37
ellensdogos <ellen.sdogos@hotmail.com>
2019-11-21 10:16:57 +0100

    Goal lists and classes

--- ClassLibrary/Goals.cs
+++ ClassLibrary/Goals.cs
@@ -3,5 +3,39 @@ using System.Collections.Generic;
 
 namespace ClassLibrary
 {
+    abstract class Goals
+    {
+        protected string type;
+        private List<Goals> completedGoals = new List<Goals>();
+        private List<Goals> goalsInProgress = new List<Goals>();
 
+        public void AddTimeGoal()
+        {
+            goalsInProgress.Add(new TimeGoal());
+        }
+
+        public void AddDistanceGoal()
+        {
+            goalsInProgress.Add(new DistanceGoal());
+        }
+        public void RemoveGoal()
+        {
+
+        }
+    }
+
+    class DistanceGoal : Goals
+    {
+        public DistanceGoal()
+        {
+            this.type = "Distance";
+        }
+    }
+    class TimeGoal : Goals
+    {
+        public TimeGoal()
+        {
+            this.type = "Time";
+        }
+    }
 }
